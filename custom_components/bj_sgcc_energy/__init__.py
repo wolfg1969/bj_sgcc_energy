76e6dfd64e074893396fa04276bed8d6adfc709b
wolfg <wolfg1969@users.noreply.github.com>
2023-07-31 00:00:54 +0800

    set update interval to one day

--- custom_components/bj_sgcc_energy/__init__.py
+++ custom_components/bj_sgcc_energy/__init__.py
@@ -13,7 +13,7 @@ from .const import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
 
-UPDATE_INTERVAL = timedelta(minutes=10)
+UPDATE_INTERVAL = timedelta(minutes=1440)  # set interval to one day
 
 
 async def async_setup(hass: HomeAssistant, hass_config):
